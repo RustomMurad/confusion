eac3427a16fd69080e0dd25a08f36afdc31069be
rlagunov <roman.lagunov@anaplan.com>
2019-01-03 19:32:20 -0800

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
 import { DISHES } from '../shared/dishes';
+import Footer from './FooterComponent';
 
 
 class Main extends Component {
@@ -23,14 +24,11 @@ class Main extends Component {
     render() {
         return (
             <div className="App">
-                <Navbar dark color="primary" data-test-navbar>
-                    <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                    </div>
-                </Navbar>
+                <Header />
                 <Menu dishes={this.state.dishes}
                     onClick={(dishId) => this.onDishSelect(dishId)} />
                 <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Footer />
             </div>
         );
     }
